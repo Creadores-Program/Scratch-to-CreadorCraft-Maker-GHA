046973a90a62be715ab479e0cde085abc6b82c92
Trollhunters501PC <cpcreador@gmail.com>
2025-03-01 13:41:19 -0600

    Añadir Nuevo Code

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ try{
   function convertCCG(htms){
     console.info(prefix+"Convert Html to Game by CreatorCraft...");
     let jsFileCache = "";
-    let cssFileCache = "iframe{ top: 0%; left: 0%; bottom: 0%; width: 100%; height: 100%; right: 0%; border: none; }";
+    let cssFileCache = "*{ position: fixed; width: 100%; height: 100%; top: 0%; left: 0%; bottom: 0%; right: 0%; border: none; }";
     let htmlFileCache = "<!DOCTYPE html><html><body><iframe  srcdoc='"+htms.replaceAll("&", "&amp;").replaceAll('"', "&#34;").replaceAll(/(\r\n|\r|\n)/g, "&#10;").replaceAll("<", "&#60;").replaceAll(">", "&#62;").replaceAll("'", "&#39;")+"'>Error! Not Load</iframe></body></html>";
     fs.writeFileSync(dirGame+"/main.js", jsFileCache);
     fs.writeFileSync(dirGame+"/index.css", cssFileCache);
